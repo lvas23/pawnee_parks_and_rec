b2bd5843546e72a09b05a1d7d6659a2c031714ad
Cory Thompson <cory.thompson98@gmail.com>
2022-02-19 12:05:26 -0600

    PORT

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const db = require('./data/db');
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -25,6 +26,6 @@ app.get('/api/db', (req, res) => {
     res.json(results);
 });
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 });
