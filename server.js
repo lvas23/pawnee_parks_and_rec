920ccdb73ef86664aa85ebb9b72c7654a5745523
Cory Thompson <cory.thompson98@gmail.com>
2022-02-20 13:10:58 -0600

    add new calendar item

--- server.js
+++ server.js
@@ -1,43 +1,59 @@
 const express = require('express');
-const db = require('./data/db');
+const calendar = require('./data/calendar');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-function filterByQuery(query, dbArray) {
-    let filteredResults = dbArray;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+function filterByQuery(query, calendarArray) {
+    let filteredResults = calendarArray;
     if (query.event) {
-        filteredResults = filteredResults.filter(db => db.event === query.event);
+        filteredResults = filteredResults.filter(calendar => calendar.event === query.event);
     }
     if (query.dayOfTheWeek) {
-        filteredResults = filteredResults.filter(db => db.dayOfTheWeek === query.dayOfTheWeek);
+        filteredResults = filteredResults.filter(calendar => calendar.dayOfTheWeek === query.dayOfTheWeek);
     }
     if (query.time) {
-        filteredResults = filteredResults.filter(db => db.time === query.time);
+        filteredResults = filteredResults.filter(calendar => calendar.time === query.time);
     }
     return filteredResults;
 }
 
-function findById(id, calendarItemsArray) {
-    const result = calendarItemsArray.filter(db = db.id === id)[0];
+function findById(id, calendarArray) {
+    const result = calendarArray.filter(calendar = calendar.id === id)[0];
     return result;
 }
 
-app.get('/api/db', (req, res) => {
-    let results = db;
+function createNewCalendar(body, calendarArray) {
+    console.log(body);
+
+
+    return body;
+}
+
+app.get('/api/calendar', (req, res) => {
+    let results = calendar;
     if (req.query) {
         results = filterByQuery(req.query, results);
     }
     res.json(results);
 });
 
-app.get('/api/db/:id', (req, res) => {
-    const result = findById(req.params.id, db);
+app.get('/api/calendar/:id', (req, res) => {
+    const result = findById(req.params.id, calendar);
     if (result) {
         res.json(result);
     } else {
         res.send(404);
     }
+});
+
+app.post('/api/calendar', (req, res) => {
+    req.body.id = calendar.length.toString();
+    
+    res.json(req.body)
 });
 
 app.listen(PORT, () => {
