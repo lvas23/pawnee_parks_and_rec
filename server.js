5ea711f9c4cc8bf9788af6207ccd804d514bb02b
Cory Thompson <cory.thompson98@gmail.com>
2022-02-21 13:41:55 -0600

    Update server.js

--- server.js
+++ server.js
@@ -6,60 +6,8 @@ const exphbs = require('express-handlebars');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static('public'));
-app.use(express.static('assets'));
-
-function filterByQuery(query, calendarArray) {
-    let filteredResults = calendarArray;
-    if (query.event) {
-        filteredResults = filteredResults.filter(calendar => calendar.event === query.event);
-    }
-    if (query.dayOfTheWeek) {
-        filteredResults = filteredResults.filter(calendar => calendar.dayOfTheWeek === query.dayOfTheWeek);
-    }
-    if (query.time) {
-        filteredResults = filteredResults.filter(calendar => calendar.time === query.time);
-    }
-    return filteredResults;
-}
-
-function findById(id, calendarArray) {
-    const result = calendarArray.filter(calendar = calendar.id === id)[0];
-    return result;
-}
-
-function createNewCalendar(body, calendarArray) {
-    console.log(body);
-    calendarArray.push(calendar);
-    fs.writeFileSync(
-        path.join(__dirname, './data/calendar.json'),
-        JSON.stringify({ calendar: calendarArray }, null, 2)
-    );
-    return calendar;
-}
-
-function validateCalendar(calendar) {
-    if (!calendar.event || typeof calendar.event !== 'string') {
-        return false;
-    }
-    if (!calendar.dayOfTheWeek || typeof calendar.dayOfTheWeek !== 'string') {
-        return false;
-    }
-    if (!calendar.time || typeof calendar.time !== 'string') {
-        return false;
-    }
-    return true;
-}
-
-app.get('/api/calendar', (req, res) => {
-    let results = calendar;
-    if (req.query) {
-        results = filterByQuery(req.query, results);
-    }
-    res.json(results);
-});
+const sequelize = require("./config/connection");
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
   secret: 'Super secret secret',
