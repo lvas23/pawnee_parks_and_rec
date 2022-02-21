f17669e5159592a2d1610be87cdd6abd3463f78e
Cory Thompson <cory.thompson98@gmail.com>
2022-02-21 12:42:38 -0600

    assets

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+app.use(express.static('assets'));
 
 function filterByQuery(query, calendarArray) {
     let filteredResults = calendarArray;
