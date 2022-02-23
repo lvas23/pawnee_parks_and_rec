9650efe947aa7284f371a9aab6e98a78ab2f6ff4
Jason Vissage <92236402+JasonVissage@users.noreply.github.com>
2022-02-22 23:56:29 -0600

    removed some files that were not needed

--- controllers/calendarRoutes.js
+++ controllers/calendarRoutes.js
@@ -1,39 +1,4 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
 
-// const {
-//   filterByQuery,
-//   findById,
-//   createNewCalendarEvent,
-//   validateDb,
-// } = require('../../lib/calendar');
-
-// router.get('/db', (req, res) => {
-//   let results = db;
-//   if (req.query) {
-//     results = filterByQuery(res.query, results);
-//   }
-//   res.json(results);
-// });
-
-// router.get('/db/:id', (req, res) => {
-//   const result = findById(req.params.id, db);
-//   if (result) {
-//     res.json(result);
-//   } else {
-//     res.send(404);
-//   }
-// });
-
-// router.post('/db', (req, res) => {
-//   req.body.id = db.length.toString();
-
-//   if (!validateDb(req.body)) {
-//     res.status(400).send('The calendar event is not properly formatted.');
-//   } else {
-//     const db = createNewCalendarEvent(req.body, db);
-//     res.json(db);
-//   }
-// });
-
 module.export = router;
--- server.js
+++ server.js
@@ -2,65 +2,12 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-// const { type } = require('express/lib/response');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/connection');
-
-// function filterByQuery(query, calendarArray) {
-//   let filteredResults = calendarArray;
-//   if (query.event) {
-//     filteredResults = filteredResults.filter(
-//       (calendar) => calendar.event === query.event
-//     );
-//   }
-//   if (query.dayOfTheWeek) {
-//     filteredResults = filteredResults.filter(
-//       (calendar) => calendar.dayOfTheWeek === query.dayOfTheWeek
-//     );
-//   }
-//   if (query.time) {
-//     filteredResults = filteredResults.filter(
-//       (calendar) => calendar.time === query.time
-//     );
-//   }
-//   return filteredResults;
-// }
-
-// function findById(id, calendarArray) {
-//   const result = calendarArray.filter((calendar = calendar.id === id))[0];
-//   return result;
-// }
-
-// function createNewCalendar(body, calendarArray) {
-//   console.log(body);
-//   calendarArray.push(calendar);
-//   fs.writeFileSync(
-//     path.join(__dirname, './data/calendar.json'),
-//     JSON.stringify({ calendar: calendarArray }, null, 2)
-//   );
-//   return calendar;
-// }
-
-// function validateCalendar(calendar) {
-//   if (!calendar.event || typeof calendar.event !== 'string') {
-//     return false;
-//   }
-//   if (!calendar.dayOfTheWeek || typeof calendar.dayOfTheWeek !== 'string') {
-//     return false;
-//   }
-//   if (!calendar.time || typeof calendar.time !== 'string') {
-//     return false;
-//   }
-//   return true;
-// }
-
-// app.get('/events', (req, res) => {
-//   res.json()
-// });
 
 const sess = {
   secret: 'Super secret secret',
