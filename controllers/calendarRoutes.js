001952373c922ecf0899b0f5cea3f3f650ac9ed9
Jason Vissage <92236402+JasonVissage@users.noreply.github.com>
2022-02-22 19:56:23 -0600

    can start server now

--- controllers/calendarRoutes.js
+++ controllers/calendarRoutes.js
@@ -1,33 +1,39 @@
-// const router = require('express').Router();
-// const { filterByQuery, findById, createNewCalendarEvent, validateDb } = require('../../lib/calendar');
-// const { db } = require('../../data/db');
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+
+// const {
+//   filterByQuery,
+//   findById,
+//   createNewCalendarEvent,
+//   validateDb,
+// } = require('../../lib/calendar');
 
 // router.get('/db', (req, res) => {
-//     let results = db;
-//     if (req.query) {
-//         results = filterByQuery(res.query, results);
-//     }
-//     res.json(results);
+//   let results = db;
+//   if (req.query) {
+//     results = filterByQuery(res.query, results);
+//   }
+//   res.json(results);
 // });
 
 // router.get('/db/:id', (req, res) => {
-//     const result = findById(req.params.id, db);
-//     if (result) {
-//         res.json(result);
-//     } else {
-//         res.send(404);
-//     }
+//   const result = findById(req.params.id, db);
+//   if (result) {
+//     res.json(result);
+//   } else {
+//     res.send(404);
+//   }
 // });
 
 // router.post('/db', (req, res) => {
-//     req.body.id = db.length.toString();
+//   req.body.id = db.length.toString();
 
-//     if (!validateDb(req.body)) {
-//         res.status(400).send('The calendar event is not properly formatted.');
-//     } else {
-//         const db = createNewCalendarEvent(req.body, db);
-//         res.json(db);
-//     }
+//   if (!validateDb(req.body)) {
+//     res.status(400).send('The calendar event is not properly formatted.');
+//   } else {
+//     const db = createNewCalendarEvent(req.body, db);
+//     res.json(db);
+//   }
 // });
 
-// module.export = router;
+module.export = router;
