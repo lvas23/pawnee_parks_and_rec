a4d9634fcd75da578443f6eb07076699c3285531
CBear <chebert222@gmail.com>
2022-02-23 14:41:30 -0700

    fixes for team to get unstuck

--- models/Post.js
+++ models/Post.js
@@ -19,7 +19,6 @@ Post.init(
   },
   {
     sequelize,
-    timestamps: false,
     freezeTableName: true,
     underscored: true,
     modelName: 'post',
--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -1,27 +1,23 @@
 const { Post } = require('../models');
 
-const postdata = [
+const postData = [
   {
-    post_id: 10,
+    post_id: 1,
     post_title: 'Donec posuere metus vitae ipsum.',
-    // post_url: 'https://buzzfeed.com/in/imperdiet/et/commodo/vulputate.png',
+
   },
   {
-    post_id: 5,
+    post_id: 2,
     post_title: 'Morbi non quam nec dui luctus rutrum.',
-    // post_url: 'https://nasa.gov/donec.json',
-    // user_id: 8,
+
   },
   {
-    post_id: 4,
+    post_id: 3,
     post_title:
       'Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue.',
-    // post_url:
-    //   'https://europa.eu/parturient/montes/nascetur/ridiculus/mus/etiam/vel.aspx',
-    // user_id: 1,
   },
 ];
 
-const seedPosts = () => Post.bulkCreate(postdata);
+const seedPosts = () => Post.bulkCreate(postData);
 
 module.exports = seedPosts;
