1b090ba75c97130246db817969d60f77775ea62d
Jason Vissage <92236402+JasonVissage@users.noreply.github.com>
2022-02-23 22:05:19 -0600

    testing paths from hbs to html

--- controllers/homepage.js
+++ controllers/homepage.js
@@ -4,19 +4,16 @@ const { Post } = require('../models');
 router.get('/', (req, res) => {
   console.log('======================');
   Post.findAll({
-    attributes: [
-      'post_id',
-      'post_title'
-    ],
+    attributes: ['post_id', 'post_title'],
   })
-    .then(dbPostData => {
-      const posts = dbPostData.map(post => post.get({ plain: true }));
+    .then((dbPostData) => {
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
 
       res.render('dashboard', {
-        posts
+        posts,
       });
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
       res.status(500).json(err);
     });
