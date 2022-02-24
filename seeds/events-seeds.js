1bf5b475ade17b3d161f6f698c7535629a7d570f
Jason Vissage <92236402+JasonVissage@users.noreply.github.com>
2022-02-23 20:52:36 -0600

    make headline and complaint div side by side

--- seeds/events-seeds.js
+++ seeds/events-seeds.js
@@ -3,17 +3,17 @@ const { Post } = require('../models');
 const postData = [
   {
     post_id: 1,
-    post_complaint:
+    post_events:
       'Sign said do not drink sprinkler water, so I made sun tea with it and now I have an infection.',
   },
   {
     post_id: 2,
-    post_complaint:
+    post_events:
       'I found a sandwich in one of your parks and I want to know why it didnt have mayonnaise.',
   },
   {
     post_id: 3,
-    post_complaint:
+    post_events:
       'My dog went to one of your parks and ate another dogs feces and Im going to sue you for that.',
   },
 ];
